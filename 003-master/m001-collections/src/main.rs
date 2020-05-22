1ee23519c96bb9baee402a50002b5fa46b8f096d
Atul Sharma <asharma@sw-at.com>
2020-05-22 21:18:00 +0100

    collections continued

--- 003-master/m001-collections/src/main.rs
+++ 003-master/m001-collections/src/main.rs
@@ -1,23 +1,66 @@
 
+use std::collections::HashMap;
 
-fn print_header(title: &str){
+fn print_header(title: &str) {
     println!("");
-    //This centers the title within a line of n - characters. 
-    //Use {:-<1$} for left alignment, and {:->1$} for right alignment. 
+    //This centers the title within a line of n - characters.
+    //Use {:-<1$} for left alignment, and {:->1$} for right alignment.
     let formatted_title = format!("{:-^1$}", title, 20);
     println!("{}", formatted_title);
     //println!("{}", (0..20).map(|_| "_").collect::<String>());
-
 }
 
 fn main() {
     arrays();
     tuples();
+    vectors();
+    hashmaps();
 }
 
-fn tuples(){
+fn hashmaps() {
+    print_header("HashMaps");
+    let mut fruits = HashMap::new();
+    fruits.insert("apple", 3);
+    fruits.insert("mango", 6);
+    fruits.insert("orange", 2);
+    fruits.insert("avocado", 7);
+
+    for (k, v) in &fruits {
+        println!("I got {} {}s", v, k);
+
+    }
+    fruits.remove("orange");
+
+    let old_avocado = fruits["avocado"];
+    fruits.insert("avocado", old_avocado +5);
+
+    println!("Now have {} avocados", fruits["avocado"]);
+    
+}
+
+fn vectors() {
+    print_header("Vectors");
+    let mut numbers_vec: Vec<u8> = Vec::new();
+    numbers_vec.push(1);
+    numbers_vec.push(2);
+
+    let mut vec_with_macro =vec![1];
+    vec_with_macro.push(2);
+
+    let message = if vec_with_macro == numbers_vec {
+        "They are equal"
+    }
+    else {
+        "Nah! They are different"
+    };
+
+    println!("{} {:?} {:?}", message, numbers_vec, vec_with_macro);
+
+}
+
+fn tuples() {
     print_header("Tuples");
-    let num_and_str: (u8, &str) =(44, "Atul Sharma");
+    let num_and_str: (u8, &str) = (44, "Atul Sharma");
     println!("{:?}", num_and_str);
     let (num, string) = num_and_str;
     println!("Destructured - Number: {}; String: {}", num, string);
@@ -25,8 +68,8 @@ fn tuples(){
 
 fn arrays() {
     print_header("Arrays");
-    let numbers:[u8; 10] =[1,2,3,4,5,6,7,8,9,10];
-    let floats =[0.1f64, 0.2, 0.3];
+    let numbers: [u8; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let floats = [0.1f64, 0.2, 0.3];
 
     println!("Number: {}", numbers[5]);
     println!("Float: {}", floats[2]);
