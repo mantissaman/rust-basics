e2fade8be7605dc4634624fcc41373c9ed839a91
Atul Sharma <asharma@sw-at.com>
2020-05-23 12:56:21 +0100

    collections and iterator done

--- 003-master/m001-collections/src/main.rs
+++ 003-master/m001-collections/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::collections::HashMap;
 
 fn print_header(title: &str) {
@@ -15,6 +14,73 @@ fn main() {
     tuples();
     vectors();
     hashmaps();
+    slices();
+    count_words();
+}
+
+#[derive(Debug)]
+struct WordCounter(HashMap<String, u64>);
+
+impl WordCounter {
+    fn new() -> Self {
+        WordCounter(HashMap::new())
+    }
+
+    fn increment(&mut self, word: &str){
+        let key = word.to_string();
+        let count = self.0.entry(key).or_insert(0);
+        *count +=1;
+    }
+
+    fn display(&self) {
+        for (key, value) in self.0.iter().filter(|&(_, v)| *v > 1 ) {
+            println!("{} : {}", key, value);
+        }
+    }
+}
+
+//use std:: env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::BufRead;
+
+fn count_words() {
+    print_header("Count Words");
+    //let args: Vec<String> = env::args().collect();
+    //let filename = &args[1];
+    let filename ="test.txt";
+    println!("Processing file: {}", filename);
+
+    let file = File::open(filename).expect("could not open file");
+    let reader = BufReader::new(file);
+
+    let mut word_counter =WordCounter::new();
+
+    for line in reader.lines(){
+        let line = line.expect("could not read line");
+        let words  = line.split(" ");
+        for word in words {
+            if word =="" {
+                continue;
+            }
+            else{
+                word_counter.increment(word);
+            }
+        }
+    }
+    word_counter.display();
+}
+
+fn slices() {
+    print_header("Slices");
+    let mut numbers: [u8; 4] = [1, 2, 3, 40];
+    let all: &[u8] = &numbers[..];
+    println!("All of them: {:?}", all);
+
+    let first_two: &mut [u8] = &mut numbers[0..2];
+    first_two[0] = 100;
+    first_two[1] = 99;
+    println!("Modified: {:?}", numbers);
 }
 
 fn hashmaps() {
@@ -27,15 +93,13 @@ fn hashmaps() {
 
     for (k, v) in &fruits {
         println!("I got {} {}s", v, k);
-
     }
     fruits.remove("orange");
 
     let old_avocado = fruits["avocado"];
-    fruits.insert("avocado", old_avocado +5);
+    fruits.insert("avocado", old_avocado + 5);
 
     println!("Now have {} avocados", fruits["avocado"]);
-    
 }
 
 fn vectors() {
@@ -44,18 +108,16 @@ fn vectors() {
     numbers_vec.push(1);
     numbers_vec.push(2);
 
-    let mut vec_with_macro =vec![1];
+    let mut vec_with_macro = vec![1];
     vec_with_macro.push(2);
 
     let message = if vec_with_macro == numbers_vec {
         "They are equal"
-    }
-    else {
+    } else {
         "Nah! They are different"
     };
 
     println!("{} {:?} {:?}", message, numbers_vec, vec_with_macro);
-
 }
 
 fn tuples() {
