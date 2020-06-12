f6b3ba5ed81b68401b95d31e46e9f60d93962273
Atul Sharma <asharma@sw-at.com>
2020-06-12 13:58:14 +0100

    sync rudis implemented...

--- 003-master/m027-advanced/rudis_sync/src/main.rs
+++ 003-master/m027-advanced/rudis_sync/src/main.rs
@@ -4,9 +4,49 @@ use std::collections::HashMap;
 use std::env;
 use std::io::{BufReader, Write};
 use std::net::Shutdown;
-//use std::
+use std::net::{TcpListener, TcpStream};
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
 
+
+mod commands;
+use crate::commands::process_client_request;
+
+type STORE  = Mutex<HashMap<String, String>>;
+
+lazy_static!{
+    static ref RUDIS_DB:  STORE = Mutex::new(HashMap::new());
+}
 
 fn main() {
-    println!("Hello, world!");
+    let addr = env::args().skip(1).next().unwrap_or("127.0.0.1:6378".to_owned());
+    let listener = TcpListener::bind(&addr).unwrap();
+    println!("rudis_sync listening on {} ... ", addr);
+
+    for stream in  listener.incoming(){
+        let stream = stream.unwrap();
+        println!("New connection from: {:?}", stream);
+        //handle_client(stream);
+
+        //can scale syncronous using threads - but there is an overhead to this
+        thread::spawn(|| handle_client(stream));
+        //thread::sleep(Duration::from_millis(3000));
+    }
+}
+fn handle_client(stream: TcpStream) {
+    let mut stream = BufReader::new(stream);
+    let decoder = Decoder::new(&mut stream).decode();
+
+    match decoder {
+        Ok(v) => {
+            let reply = process_client_request(v);
+            stream.get_mut().write_all(&reply).unwrap();
+        }
+        Err(e) => {
+            println!("Invalid command: {:?}", e);
+            let _= stream.get_mut().shutdown(Shutdown::Both);
+        }
+    }
+
 }
