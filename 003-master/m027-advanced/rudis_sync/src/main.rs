18b13723a43a33cef4d6023e3f9f426ae5ddf7cf
Atul Sharma <asharma@sw-at.com>
2020-06-12 12:54:16 +0100

    rudis setup

---
+++ 003-master/m027-advanced/rudis_sync/src/main.rs
@@ -0,0 +1,12 @@
+use lazy_static::lazy_static;
+use resp::Decoder;
+use std::collections::HashMap;
+use std::env;
+use std::io::{BufReader, Write};
+use std::net::Shutdown;
+//use std::
+
+
+fn main() {
+    println!("Hello, world!");
+}
