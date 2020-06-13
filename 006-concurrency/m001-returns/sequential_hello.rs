0cdd5f8def90f4bcacaf728ade6f05bcca7b12bc
Atul Sharma <asharma@sw-at.com>
2020-06-13 19:39:14 +0100

    timing stuff need to be tried on linux - not working on mac

---
+++ 006-concurrency/m001-returns/sequential_hello.rs
@@ -0,0 +1,6 @@
+fn main(){
+    println!("Greetings, Human!");
+}
+//brew install gnu-time
+//rustc -C opt-level=3 sequential_hello.rs
+//time for i in {1..100}; do ./sequential_hello > /dev/null; done
