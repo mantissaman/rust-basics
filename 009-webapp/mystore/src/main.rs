4dbf6fd30892e0de52d353f7356f1c3ced081cde
Atul Sharma <asharma@sw-at.com>
2020-06-24 20:38:46 +0100

    adding routes

--- 009-webapp/mystore/src/main.rs
+++ 009-webapp/mystore/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+pub mod schema;
+pub mod db_connection;
+pub mod models;
+pub mod handlers;
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate serde;
+extern crate serde_json;
+#[macro_use] 
+extern crate serde_derive;
+
+use actix_web::{HttpServer, App, web, get, HttpRequest, HttpResponse};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(
+        web::resource("/").route(web::get().to(handlers::products::index)
+    ))
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run().await
 }
